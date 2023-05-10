b181f21d0eda2c58e3de5eb6e983c5df10404981
Aleksey Goryn <fin.07@bk.ru>
2023-05-10 16:21:55 +0300

    template

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,77 @@
+<template>
+	<div>
+        <p class="mt-0 mb-1 font-bold uppercase text-slate-400">Урок {{ chapter.number }} - {{ lesson.number }}</p>
+
+        <h2 class="my-0">{{ lesson.title }}</h2>
+
+        <div class="flex mt-2 mb-8 space-x-4">
+			<NuxtLink
+				v-if="lesson.sourceUrl"
+				class="font-normal text-gray-500 text-md"
+				:to="lesson.sourceUrl"
+                target="_blank"
+			>
+                Скачать исходный код
+			</NuxtLink>
+
+			<NuxtLink
+				v-if="lesson.downloadUrl"
+				class="font-normal text-gray-500 text-md"
+				:to="lesson.downloadUrl"
+                target="_blank"
+			>
+                Скачать видео
+			</NuxtLink>
+		</div>
+        
+		<p>{{ lesson.text }}</p>
+
+        <VideoPlayer v-if="lesson.videoId" :video-id="lesson.videoId"/>
+
+        <LessonCompleteButton :model-value="isLessonComplete" @update:model-value="toggleComplete" />
+    </div>
+</template>
+
+<script setup>
+const course = useCourse();
+const route= useRoute();
+
+const chapter = computed(() => {
+    return course.chapters.find(chapter => chapter.slug === route.params.chapterSlug);
+});
+
+const lesson = computed(() => {
+    return chapter.value.lessons.find(lesson => lesson.slug === route.params.lessonSlug);
+});
+
+const title = computed(() => {
+	return `${lesson.value.title} - ${course.title}`;
+});
+
+useHead({
+	title,
+});
+
+const progress = useLocalStorage('progress', []);
+
+const isLessonComplete = computed(() => {
+	if (!progress.value[chapter.value.number - 1]) {
+		return false;
+	}
+
+	if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+		return false;
+	}
+
+	return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+});
+
+const toggleComplete = () => {
+	if (!progress.value[chapter.value.number - 1]) {
+		progress.value[chapter.value.number - 1] = [];
+	}
+
+	progress.value[chapter.value.number - 1][lesson.value.number - 1] =
+		!isLessonComplete.value;
+};
+</script>
