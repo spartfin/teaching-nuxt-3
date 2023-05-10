d65358592f7047fb948444ea250d796acdb68d64
Aleksey Goryn <fin.07@bk.ru>
2023-05-10 12:30:51 +0300

    template

---
+++ pages/course.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="p-12 bg-gray-100 w-full  min-h-screen flex flex-col items-center">
+        <div class="prose mb-12">
+            <h1>
+                <span class="font-medium">
+                    Курс:
+
+                    <span class="font-bold">Учим Nuxt 3</span>
+                </span>
+            </h1>
+        </div>
+
+        <div class="flex flex-row justify-center flex-grow">
+            <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col">
+                <h3>Главы</h3>
+            </div>
+
+            <div class="prose mr-4 p-8 bg-white rounded-md w-[65ch]">
+                <h1>Урок</h1>
+
+                <p>Это урок</p>
+            </div>
+        </div>
+    </div>
+</template>
