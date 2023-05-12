ba0da98b0628cf6e8835675fc749930df005dbb6
Aleksey Goryn <fin.07@bk.ru>
2023-05-12 15:15:40 +0300

    template

--- pages/login.vue
+++ pages/login.vue
@@ -2,10 +2,21 @@
     <div class="prose w-full max-w-2xl h-9">
         <h1>Войти {{ title }}</h1>
 
-        <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">Войти через Github</button>
+        <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded" @click="login">Войти через Github</button>
     </div>
 </template>
 
 <script setup lang="ts">
 const { title } = useCourse();
+const supabase = useSupabaseClient();
+
+const login = async () => {
+const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+});
+
+if (error) {
+    console.error(error);
+}
+};
 </script>
